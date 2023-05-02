52072d8daf1ee9910517a00eb6a0406879b461bd
Treit91 <119440239+Treit91@users.noreply.github.com>
2023-05-02 10:37:19 +0200

    add mongoDB connection

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.listen(3001)
 
+mongoose.connect("mongodb+srv://treit91:S4e12ipFD5hG18qa@cluster0.zjzcwnl.mongodb.net/test")
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -10,4 +11,5 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
     next();
 });
+
 app.use(express.json());
