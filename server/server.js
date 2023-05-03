72a39e71257c9b9418a1f27b4f2ad0f693f54c6c
Treit91 <119440239+Treit91@users.noreply.github.com>
2023-05-03 12:40:38 +0200

    add server setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./models/User');
 const app = express();
 app.listen(3001)
 
 mongoose.connect("mongodb+srv://treit91:S4e12ipFD5hG18qa@cluster0.zjzcwnl.mongodb.net/test")
 
+app.use(express.json());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -12,4 +14,60 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(express.json());
+app.post("/api/register", async (req, res) => {
+    console.log('req.body', req.body)
+    const usernameFound = await User.findOne({ username: req.body.username });
+    const emailFound = await User.findOne({ email: req.body.email });
+    if (usernameFound) {
+        res.json("username exists").status(500);
+    } else if (emailFound) {
+        res.json("email exists").statusz(500);
+    } else {
+        try {
+            User.create({
+                name: {
+                    first: req.body.firstName,
+                    last: req.body.lastName
+                },
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                registeredAt: Date.now()
+            })
+            res.json("ok").status(201);
+        } catch (error) {
+            console.error(error.message);
+            res.sendStatus(500);
+        }
+    }
+})
+
+app.post('/api/login', async (req, res) => {
+    const usernameFound = await User.findOne({ username: req.body.username });
+
+    if (usernameFound) {
+        if (usernameFound.password === req.body.password) {
+            res.json({ succes: true, user: usernameFound });
+        } else {
+            res.json("Incorrect password!");
+        }
+    } else {
+        res.json("Username doesn't exists!").status(500);
+    }
+})
+
+app.get('/api/users', async (req, res) => {
+    try {
+        const response = await User.find({});
+        res.json(response)
+    } catch (err) {
+        console.error(err.message);
+        res.sendStatus(500);
+    }
+})
+
+app.patch('/user/:username', (req, res) => {
+    User.findOneAndUpdate({ username: req.params.username }, { username: req.body.username, password: req.body.password }, { new: true })
+        .then(result => res.status(200).json(result))
+        .catch(error => console.error(error))
+})
